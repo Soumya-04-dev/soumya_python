43108c72282789f8d7011297aa4a802e3eb6055c
Soumya <Soumya.cric4@gmail.com>
2022-03-04 08:05:14 +0530

    "Day 9 code commits"

---
+++ Day9/list_of_requests_day904032022.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# Github Token: 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
+
+'''
+for user_create_repo in range(2):
+    repository_name = input("Enter the name of the repository ")
+    data = {"name": f"{repository_name}"}
+    requests.post(url, data=json.dumps(data), headers=headers)
+'''
+url = "https://api.github.com/users/Soumya-04-dev/repos"
+token = "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"
+headers = {"Authorization": f'token {token}'}
+response = requests.get(url, headers=headers)
+print(response.text)
+# str -> dict --json.loads()
+data = json.loads(response.text)
+for i in data:
+    print(i["name"])
