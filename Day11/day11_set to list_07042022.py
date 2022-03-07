1f6754d2703f40c3bb8e6b475c8be19c19ffe235
Soumya <Soumya.cric4@gmail.com>
2022-03-07 09:35:58 +0530

    "Day 11 code commits_ Conversion from list to dict and dict to list code"

---
+++ Day11/day11_set to list_07042022.py
@@ -0,0 +1,22 @@
+# a = {1,2,3}
+# b= list(a)
+# print(b)
+
+# c = tuple(a)
+# print(c)
+
+# a = {1,2,3,1,1,1} # set will not keep duplicate items.
+# print(a)
+
+'''
+a = [1, 2, 3, 4, 2, 2, 3, 4]
+print(a)
+b= set(a)
+print(b)
+print((list(b)))
+
+
+a = {1,2,3,4}
+b = {4,5,6,4}
+print(a.union(b))
+print(a.intersection(b))'''
