db86e6ca4b414605a6af91c24a45cb8efeef4f0c
soumya <soumya.cric4@gmail.com>
2022-03-17 08:50:34 +0530

    "Day 18 code commits_ coding"

--- Day18/src/day18_function_exam_16032022.py
+++ Day18/src/day18_function_exam_16032022.py
@@ -1,20 +1,28 @@
-import selenium
+import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(executable_path='/home/sohil/soumya_python/soumya_python/Day18/src/Browser/chromedriver')
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 def navigate_url():
     print(f'open chrome browser')
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome('/home/sohil/soumya_python/soumya_python/Day18/src/Browser/chromedriver')
+    time.sleep(2)
     print(f'maximize the chrome browser window')
     driver.maximize_window()
+    time.sleep(2)
     print(f'navigate to the url')
+    driver.get("https://www.google.com/")
+    time.sleep(2)
+    print(f'Enter the text')
+    driver.find_element(by=By.NAME, value="q").send_keys('facebook')
+    time.sleep(2)
+    print(f'Click on Enter button')
+    driver.find_element(by=By.NAME, value="btnK").send_keys(Keys.ENTER)
+    time.sleep(2)
+    print(f'Close the browser')
+    driver.close()
 
-
-#   driver.get("https://www.google.com/")
 
 
 navigate_url()
